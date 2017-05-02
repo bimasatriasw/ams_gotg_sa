2f73decdad973b5c1f17d17d7dae6d5eb7bc0b16
Bima Satria <bimassw@gmail.com>
2017-05-02 20:20:54 +0700

    bikin struktur file utamanya (jadi file utamanya itu yang mining ini guys, nah nanti dia pake method2 yg ada di util)

---
+++ mining.py
@@ -0,0 +1,14 @@
+import util
+
+# 1. DATA SET EXTRACTION
+dataset = util.extract('dataset/twitter')
+
+
+# 2. PRE-PROCESSING
+bags_of_words = util.tokenize(dataset)
+
+
+# 3. REPRESENTATION
+
+
+# 4. BUILDING MODEL
