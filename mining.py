d062da1be1cf279f04f59c072470361812b94e81
Bima Satria <bimassw@gmail.com>
2017-05-08 22:09:45 +0700

    dirubah tanpa tokenisasi + udah ada vectorizernya juga. (model blm ada soalnya bingung takut error kan belom ada labelnya)

--- mining.py
+++ mining.py
@@ -1,14 +1,37 @@
 import util
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 
 # 1. DATA SET EXTRACTION
-dataset = util.extract('dataset/twitter')
+
+dataset = util.extract('dataset/twitter-full')
 
 
 # 2. PRE-PROCESSING
-bags_of_words = util.tokenize(dataset)
+
+dataset = util.filter_stopwords(dataset)
+content = np.array(dataset)
 
 
-# 3. REPRESENTATION
+# 3. REPRESENTATION (kemungkinan bentuk vektor dengan mungkin ada tambahan fitur lain)
+
+# # Pilihan metode representasi vektor # #
+# vectorizer = CountVectorizer()
+vectorizer = TfidfVectorizer()
+vector = vectorizer.fit_transform(content[:, 2])
 
 
 # 4. BUILDING MODEL
+
+
+
+
+
+
+
+# # FOR CHECKING
+# print content[:, 2]
+# for v in vector:
+# 	print v
